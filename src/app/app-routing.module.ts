3bcc37345748a55563615ed2af9244dff60cbf4a
Daniel Leonardo Romero Albarracin <daniel.romero@DESKTOP-2TO21MO.pragma.com.co>
2020-09-16 08:20:56 -0500

    Update folders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './home/dashboard/dashboard.component';
-import { ManageIterationsComponent } from './home/manage-iterations/manage-iterations.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ManageIterationsComponent } from './manage-iterations/manage-iterations.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidenavComponent } from './home/sidenav/sidenav.component';
-import { DashboardComponent } from './home/dashboard/dashboard.component';
-import { ManageIterationsComponent } from './home/manage-iterations/manage-iterations.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ManageIterationsComponent } from './manage-iterations/manage-iterations.component';
 
 @NgModule({
   declarations: [

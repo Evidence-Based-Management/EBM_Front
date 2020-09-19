c4e59d369fc0c5f3b35e4a800304d7719d67e2e7
Daniel <raiga1234@gmail.com>
2020-09-19 12:12:58 -0500

    add unit test to ensure link redirect exist and has the correct value

--- src/app/home/sidenav/sidenav.component.spec.ts
+++ src/app/home/sidenav/sidenav.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { SidenavComponent } from './sidenav.component';
 
@@ -8,9 +9,8 @@ describe('SidenavComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SidenavComponent ]
-    })
-    .compileComponents();
+      declarations: [SidenavComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +21,19 @@ describe('SidenavComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have Dashboard and Manage Iterations links', () => {
+    const result = fixture.debugElement.queryAll(By.css('span'));
+
+    expect(result[0].nativeElement.innerHTML).toBe('Dashboard');
+    expect(result[1].nativeElement.innerHTML).toBe('Manage Iterations');
+  });
+
+  it('should have the arribute routerLink /dashboard and /iterations', () => {
+    const result = fixture.debugElement.queryAll(By.css('.nav-link'));
+
+    expect(result[0].attributes.routerLink).toBe('/dashboard');
+    expect(result[1].attributes.routerLink).toBe('/iterations');
   });
 });

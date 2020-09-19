d9760a9af5e36ff99d378e9f77fa6f8ae26a767e
Daniel <raiga1234@gmail.com>
2020-09-18 21:10:52 -0500

    Remove code smells sonarqube

--- src/app/manage-iterations/iteration/iteration.component.ts
+++ src/app/manage-iterations/iteration/iteration.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Iteration } from 'src/app/Interfaces/iterations';
 
 @Component({
   selector: 'app-iteration',

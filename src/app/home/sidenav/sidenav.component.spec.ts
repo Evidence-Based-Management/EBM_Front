87a24172b0328dabe57c01d6e77b4ee2ad15f399
Daniel Leonardo Romero Albarracin <daniel.romero@DESKTOP-2TO21MO.pragma.com.co>
2020-09-22 18:25:00 -0500

    Change skin

--- src/app/home/sidenav/sidenav.component.spec.ts
+++ src/app/home/sidenav/sidenav.component.spec.ts
@@ -27,13 +27,13 @@ describe('SidenavComponent', () => {
     const result = fixture.debugElement.queryAll(By.css('span'));
 
     expect(result[0].nativeElement.innerHTML).toBe('Dashboard');
-    expect(result[1].nativeElement.innerHTML).toBe('Manage Iterations');
+    expect(result[1].nativeElement.innerHTML).toBe('Iterations');
   });
 
   it('should have the arribute routerLink /dashboard and /iterations', () => {
-    const result = fixture.debugElement.queryAll(By.css('.nav-link'));
+    const result = fixture.debugElement.queryAll(By.css('a'));
 
-    expect(result[0].attributes.routerLink).toBe('/dashboard');
-    expect(result[1].attributes.routerLink).toBe('/iterations');
+    expect(result[1].attributes.routerLink).toBe('/dashboard');
+    expect(result[3].attributes.routerLink).toBe('/iterations');
   });
 });

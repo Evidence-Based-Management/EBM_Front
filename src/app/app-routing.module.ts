ad098242d2f13c885a01d34f5832d52fc460e289
Daniel Leonardo Romero Albarracin <daniel.romero@DESKTOP-2TO21MO.pragma.com.co>
2020-09-25 14:44:03 -0500

    hack to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { IterationComponent } from './manage-iterations/iteration/iteration.component';
 import { ManageIterationsComponent } from './manage-iterations/manage-iterations.component';
@@ -9,6 +10,7 @@ const routes: Routes = [
   { path: 'iterations', component: ManageIterationsComponent },
   { path: 'iteration/:id', component: IterationComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', component: AppComponent },
 ];
 
 @NgModule({

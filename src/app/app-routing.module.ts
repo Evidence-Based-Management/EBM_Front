9ae59d05faa2ae81c64580eff29f8d276f4732b6
Daniel <raiga1234@gmail.com>
2020-09-28 22:19:15 -0500

    Remove unsuded import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { IterationComponent } from './manage-iterations/iteration/iteration.component';
 import { ManageIterationsComponent } from './manage-iterations/manage-iterations.component';

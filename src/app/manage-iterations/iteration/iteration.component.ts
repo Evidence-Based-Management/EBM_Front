e8605cda9a5a9fb50f9b58a15edf01979d8340c6
Daniel <raiga1234@gmail.com>
2020-12-10 22:55:33 -0500

    Refactor validation Iteration component

--- src/app/manage-iterations/iteration/iteration.component.ts
+++ src/app/manage-iterations/iteration/iteration.component.ts
@@ -29,7 +29,6 @@ export class IterationComponent implements OnInit {
   }
 
   mapIteration(response: any): void {
-    
     if ('id' in response && 'kva' in response) {
       this.iteration = {
         id: response.id,
@@ -40,105 +39,82 @@ export class IterationComponent implements OnInit {
         state: response.state,
         KVM: {
           A2I: {
-            Active_Code_Branches:
-              'kvaAbilityToInnovate' in response.kva
-                ? response.kva.kvaAbilityToInnovate
-                    .activeCodeBranchesTimeSpentMergingCodeBetweenBranches
-                : '',
-            Defect_Trends:
-              'kvaAbilityToInnovate' in response.kva
-                ? response.kva.kvaAbilityToInnovate.defectTrends
-                : '',
-            Feature_Usage_Index:
-              'kvaAbilityToInnovate' in response.kva
-                ? response.kva.kvaAbilityToInnovate.featureUsageIndex
-                : [],
-            Innovation_Rate:
-              'kvaAbilityToInnovate' in response.kva
-                ? response.kva.kvaAbilityToInnovate.innovationRate
-                : '',
-            Installed_Version_Index:
-              'kvaAbilityToInnovate' in response.kva
-                ? response.kva.kvaAbilityToInnovate.installedVersionIndex
-                : '',
-            On_Product_Index:
-              'kvaAbilityToInnovate' in response.kva
-                ? response.kva.kvaAbilityToInnovate.onProductIndex
-                : '',
-            Production_Incident_Trends:
-              'kvaAbilityToInnovate' in response.kva
-                ? response.kva.kvaAbilityToInnovate.productionIncidentTrends
-                : '',
-            Technical_Debt:
-              'kvaAbilityToInnovate' in response.kva
-                ? response.kva.kvaAbilityToInnovate.technicalDebt
-                : '',
-            Time_Spent_Context_Switching:
-              'kvaAbilityToInnovate' in response.kva
-                ? response.kva.kvaAbilityToInnovate.timeSpentContextSwitching
-                : '',
+            Active_Code_Branches: response?.kva?.kvaAbilityToInnovate
+              ? response.kva.kvaAbilityToInnovate
+                  .activeCodeBranchesTimeSpentMergingCodeBetweenBranches
+              : '',
+            Defect_Trends: response?.kva?.kvaAbilityToInnovate
+              ? response.kva.kvaAbilityToInnovate.defectTrends
+              : '',
+            Feature_Usage_Index: response?.kva?.kvaAbilityToInnovate
+              ? response.kva.kvaAbilityToInnovate.featureUsageIndex
+              : [],
+            Innovation_Rate: response?.kva?.kvaAbilityToInnovate
+              ? response.kva.kvaAbilityToInnovate.innovationRate
+              : '',
+            Installed_Version_Index: response?.kva?.kvaAbilityToInnovate
+              ? response.kva.kvaAbilityToInnovate.installedVersionIndex
+              : '',
+            On_Product_Index: response?.kva?.kvaAbilityToInnovate
+              ? response.kva.kvaAbilityToInnovate.onProductIndex
+              : '',
+            Production_Incident_Trends: response?.kva?.kvaAbilityToInnovate
+              ? response.kva.kvaAbilityToInnovate.productionIncidentTrends
+              : '',
+            Technical_Debt: response?.kva?.kvaAbilityToInnovate
+              ? response.kva.kvaAbilityToInnovate.technicalDebt
+              : '',
+            Time_Spent_Context_Switching: response?.kva?.kvaAbilityToInnovate
+              ? response.kva.kvaAbilityToInnovate.timeSpentContextSwitching
+              : '',
           },
           CV: {
-            Customer_Satisfaction:
-              'kvaCurrentValue' in response.kva
-                ? response.kva.kvaCurrentValue.customerSatisfaction
-                : '',
-            Customer_Usage_Index:
-              'kvaCurrentValue' in response.kva
-                ? response.kva.kvaCurrentValue.customerUsageIndex
-                : '',
-            Employee_Satisfaction:
-              'kvaCurrentValue' in response.kva
-                ? response.kva.kvaCurrentValue.employeeSatisfaction
-                : '',
-            Product_Cost_Ratio:
-              'kvaCurrentValue' in response.kva
-                ? response.kva.kvaCurrentValue.productCostRatio
-                : '',
-            Revenue_Per_Employee:
-              'kvaCurrentValue' in response.kva
-                ? response.kva.kvaCurrentValue.revenuePerEmployee
-                : '',
+            Customer_Satisfaction: response?.kva?.kvaCurrentValue
+              ? response.kva.kvaCurrentValue.customerSatisfaction
+              : '',
+            Customer_Usage_Index: response?.kva?.kvaCurrentValue
+              ? response.kva.kvaCurrentValue.customerUsageIndex
+              : '',
+            Employee_Satisfaction: response?.kva?.kvaCurrentValue
+              ? response.kva.kvaCurrentValue.employeeSatisfaction
+              : '',
+            Product_Cost_Ratio: response?.kva?.kvaCurrentValue
+              ? response.kva.kvaCurrentValue.productCostRatio
+              : '',
+            Revenue_Per_Employee: response?.kva?.kvaCurrentValue
+              ? response.kva.kvaCurrentValue.revenuePerEmployee
+              : '',
           },
           T2M: {
-            Build_And_Integration_Frequency:
-              'kvaTimeToMarket' in response.kva
-                ? response.kva.kvaTimeToMarket.buildAndIntegrationFrequency
-                : '',
-            Cycle_Time:
-              'kvaTimeToMarket' in response.kva
-                ? response.kva.kvaTimeToMarket.cycleTime
-                : '',
-            Lead_Time:
-              'kvaTimeToMarket' in response.kva
-                ? response.kva.kvaTimeToMarket.leadTime
-                : '',
-            Mean_Time_To_Repair:
-              'kvaTimeToMarket' in response.kva
-                ? response.kva.kvaTimeToMarket.meanTimeToRepair
-                : '',
-            Release_Frequency:
-              'kvaTimeToMarket' in response.kva
-                ? response.kva.kvaTimeToMarket.releaseFrequency
-                : '',
-            Release_Stabilization_Period:
-              'kvaTimeToMarket' in response.kva
-                ? response.kva.kvaTimeToMarket.releaseStabilizationPeriod
-                : '',
-            Time_To_Learn:
-              'kvaTimeToMarket' in response.kva
-                ? response.kva.kvaTimeToMarket.timeToLearn
-                : '',
+            Build_And_Integration_Frequency: response?.kva?.kvaTimeToMarket
+              ? response.kva.kvaTimeToMarket.buildAndIntegrationFrequency
+              : '',
+            Cycle_Time: response?.kva?.kvaTimeToMarket
+              ? response.kva.kvaTimeToMarket.cycleTime
+              : '',
+            Lead_Time: response?.kva?.kvaTimeToMarket
+              ? response.kva.kvaTimeToMarket.leadTime
+              : '',
+            Mean_Time_To_Repair: response?.kva?.kvaTimeToMarket
+              ? response.kva.kvaTimeToMarket.meanTimeToRepair
+              : '',
+            Release_Frequency: response?.kva?.kvaTimeToMarket
+              ? response.kva.kvaTimeToMarket.releaseFrequency
+              : '',
+            Release_Stabilization_Period: response?.kva?.kvaTimeToMarket
+              ? response.kva.kvaTimeToMarket.releaseStabilizationPeriod
+              : '',
+            Time_To_Learn: response?.kva?.kvaTimeToMarket
+              ? response.kva.kvaTimeToMarket.timeToLearn
+              : '',
           },
           UV: {
-            Customer_Or_User_Satisfaction_Gap:
-              'kvaUnrealizedValue' in response.kva
-                ? response.kva.kvaUnrealizedValue.customerSatisfactionGap
-                : '',
-            Market_Share:
-              'kvaUnrealizedValue' in response.kva
-                ? response.kva.kvaUnrealizedValue.marketShare
-                : '',
+            Customer_Or_User_Satisfaction_Gap: response?.kva?.kvaUnrealizedValue
+              ? response.kva.kvaUnrealizedValue.customerSatisfactionGap
+              : '',
+            Market_Share: response?.kva?.kvaUnrealizedValue
+              ? response.kva.kvaUnrealizedValue.marketShare
+              : '',
           },
         },
       };

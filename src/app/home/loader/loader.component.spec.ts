b5c3080661abb5b8f3a8bb5aaf7895722f91cda5
Daniel <raiga1234@gmail.com>
2020-12-16 20:55:01 -0500

    Add unit testing loader component

--- src/app/home/loader/loader.component.spec.ts
+++ src/app/home/loader/loader.component.spec.ts
@@ -1,25 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { LoaderState } from 'src/app/Interfaces/loader';
+import { LoaderService } from 'src/app/services/loader.service';
 
 import { LoaderComponent } from './loader.component';
 
 describe('LoaderComponent', () => {
   let component: LoaderComponent;
   let fixture: ComponentFixture<LoaderComponent>;
+  let loaderService: LoaderService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoaderComponent ]
-    })
-    .compileComponents();
+      declarations: [LoaderComponent],
+      providers: [
+        {
+          provide: LoaderService,
+          useValue: {
+            loaderState: () => of({ show: true } as LoaderState),
+          },
+        },
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoaderComponent);
+    loaderService = TestBed.inject(LoaderService);
+    loaderService.loaderState = of({ show: true } as LoaderState);
+
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component.show).toBeTrue();
   });
 });

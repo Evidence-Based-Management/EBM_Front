b9c676c53873b8c82dbf587169128d83c920cd16
Daniel <raiga1234@gmail.com>
2020-12-22 20:39:21 -0500

    Fix menu problem include jquery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Evidence Based Management';
+  ngOnInit(): void {
+    this.loadScript('../assets/js/menu.js');
+  }
+
+  loadScript(url: string): void {
+    const body = <HTMLDivElement>document.body;
+    const script = document.createElement('script');
+    script.innerHTML = '';
+    script.src = url;
+    script.async = false;
+    script.defer = true;
+    body.appendChild(script);
+  }
 }

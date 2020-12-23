09cf477562032c9359ba603c52b2d4523f86119c
Daniel <raiga1234@gmail.com>
2020-12-22 21:24:16 -0500

    Fix unit test and fix jquery dependency in unit test

--- src/app/home/sidenav/sidenav.component.spec.ts
+++ src/app/home/sidenav/sidenav.component.spec.ts
@@ -26,14 +26,14 @@ describe('SidenavComponent', () => {
   it('should have Dashboard and Manage Iterations links', () => {
     const result = fixture.debugElement.queryAll(By.css('span'));
 
-    expect(result[0].nativeElement.innerHTML).toBe('Dashboard');
-    expect(result[1].nativeElement.innerHTML).toBe('Iterations');
+    expect(result[0].nativeElement.innerHTML).toBe('Iterations');
+    expect(result[1].nativeElement.innerHTML).toBe('Dashboard');
   });
 
   it('should have the arribute routerLink /dashboard and /iterations', () => {
     const result = fixture.debugElement.queryAll(By.css('a'));
 
-    expect(result[1].attributes.routerLink).toBe('/dashboard');
-    expect(result[3].attributes.routerLink).toBe('/iterations');
+    expect(result[1].attributes.routerLink).toBe('/iterations');
+    expect(result[2].attributes.routerLink).toBe('/dashboard');
   });
 });

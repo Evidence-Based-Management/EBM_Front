f1d63b0ecc66d73b56c937ae6f3fc6494756acac
Daniel <raiga1234@gmail.com>
2020-12-30 18:15:48 -0500

    Abstract maptoIterations to a abstract class

--- src/app/manage-iterations/iteration/iteration.component.ts
+++ src/app/manage-iterations/iteration/iteration.component.ts
@@ -8,6 +8,7 @@ import { KVACurrentValueService } from '../../services/kvacurrent-value.service'
 import { forkJoin, Observable } from 'rxjs';
 import { KVATimeToMarketService } from '../../services/kvatime-to-market.service';
 import { KVAAbilityToInnovateService } from '../../services/kvaability-to-innovate.service';
+import { IterationMappers } from 'src/app/utilities/mappers';
 
 @Component({
   selector: 'app-iteration',
@@ -84,111 +85,8 @@ export class IterationComponent implements OnInit {
     this.serviceItertations
       .getIterationById(this.idIteration)
       .subscribe((response: any) => {
-        this.mapToIteration(response);
+        this.iteration = IterationMappers.mapToIteration(response);
       });
-  }
-
-  mapToIteration(response: any): void {
-    this.iteration = {
-      id: response?.id ? response.id : '',
-      name: response?.name ? response.name : '',
-      goal: response?.goal ? response.goal : '',
-      startDate: response?.startDate ? response.startDate : '',
-      endDate: response?.endDate ? response.endDate : '',
-      state: response?.state ? response.state : '',
-      KVM: {
-        A2I: {
-          id: response?.kva?.kvaAbilityToInnovate
-            ? response.kva.kvaAbilityToInnovate.id
-            : '',
-          Active_Code_Branches: response?.kva?.kvaAbilityToInnovate
-            ? response.kva.kvaAbilityToInnovate
-                .activeCodeBranchesTimeSpentMergingCodeBetweenBranches
-            : '',
-          Defect_Trends: response?.kva?.kvaAbilityToInnovate
-            ? response.kva.kvaAbilityToInnovate.defectTrends
-            : '',
-          Feature_Usage_Index: response?.kva?.kvaAbilityToInnovate
-            ? response.kva.kvaAbilityToInnovate.featureUsageIndex
-            : '',
-          Innovation_Rate: response?.kva?.kvaAbilityToInnovate
-            ? response.kva.kvaAbilityToInnovate.innovationRate
-            : '',
-          Installed_Version_Index: response?.kva?.kvaAbilityToInnovate
-            ? response.kva.kvaAbilityToInnovate.installedVersionIndex
-            : '',
-          On_Product_Index: response?.kva?.kvaAbilityToInnovate
-            ? response.kva.kvaAbilityToInnovate.onProductIndex
-            : '',
-          Production_Incident_Trends: response?.kva?.kvaAbilityToInnovate
-            ? response.kva.kvaAbilityToInnovate.productionIncidentTrends
-            : '',
-          Technical_Debt: response?.kva?.kvaAbilityToInnovate
-            ? response.kva.kvaAbilityToInnovate.technicalDebt
-            : '',
-          Time_Spent_Context_Switching: response?.kva?.kvaAbilityToInnovate
-            ? response.kva.kvaAbilityToInnovate.timeSpentContextSwitching
-            : '',
-        },
-        CV: {
-          id: response?.kva?.kvaCurrentValue
-            ? response.kva.kvaCurrentValue.id
-            : '',
-          Customer_Satisfaction: response?.kva?.kvaCurrentValue
-            ? response.kva.kvaCurrentValue.customerSatisfaction
-            : '',
-          Customer_Usage_Index: response?.kva?.kvaCurrentValue
-            ? response.kva.kvaCurrentValue.customerUsageIndex
-            : '',
-          Employee_Satisfaction: response?.kva?.kvaCurrentValue
-            ? response.kva.kvaCurrentValue.employeeSatisfaction
-            : '',
-          Product_Cost_Ratio: response?.kva?.kvaCurrentValue
-            ? response.kva.kvaCurrentValue.productCostRatio
-            : '',
-          Revenue_Per_Employee: response?.kva?.kvaCurrentValue
-            ? response.kva.kvaCurrentValue.revenuePerEmployee
-            : '',
-        },
-        T2M: {
-          id: response?.kva?.kvaTimeToMarket
-            ? response.kva.kvaTimeToMarket.id
-            : '',
-          Build_And_Integration_Frequency: response?.kva?.kvaTimeToMarket
-            ? response.kva.kvaTimeToMarket.buildAndIntegrationFrequency
-            : '',
-          Cycle_Time: response?.kva?.kvaTimeToMarket
-            ? response.kva.kvaTimeToMarket.cycleTime
-            : '',
-          Lead_Time: response?.kva?.kvaTimeToMarket
-            ? response.kva.kvaTimeToMarket.leadTime
-            : '',
-          Mean_Time_To_Repair: response?.kva?.kvaTimeToMarket
-            ? response.kva.kvaTimeToMarket.meanTimeToRepair
-            : '',
-          Release_Frequency: response?.kva?.kvaTimeToMarket
-            ? response.kva.kvaTimeToMarket.releaseFrequency
-            : '',
-          Release_Stabilization_Period: response?.kva?.kvaTimeToMarket
-            ? response.kva.kvaTimeToMarket.releaseStabilizationPeriod
-            : '',
-          Time_To_Learn: response?.kva?.kvaTimeToMarket
-            ? response.kva.kvaTimeToMarket.timeToLearn
-            : '',
-        },
-        UV: {
-          id: response?.kva?.kvaUnrealizedValue
-            ? response.kva.kvaUnrealizedValue.id
-            : '',
-          Customer_Or_User_Satisfaction_Gap: response?.kva?.kvaUnrealizedValue
-            ? response.kva.kvaUnrealizedValue.customerSatisfactionGap
-            : '',
-          Market_Share: response?.kva?.kvaUnrealizedValue
-            ? response.kva.kvaUnrealizedValue.marketShare
-            : '',
-        },
-      },
-    };
   }
 
   mapFromIteration(iteration: Iteration, newState: string): any {

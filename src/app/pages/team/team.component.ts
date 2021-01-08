c82882265449b9611254d596d9fda678de5fa5ab
Daniel <raiga1234@gmail.com>
2021-01-08 16:11:45 -0500

    remove code smells

--- src/app/pages/team/team.component.ts
+++ src/app/pages/team/team.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
-  styleUrls: ['./team.component.css']
+  styleUrls: []
 })
 export class TeamComponent implements OnInit {
 
